afdd5dd3953adad5e89ad2d9f723ba9b43c0e08c
tanvirs2 <tanvir603@gmail.com>
2023-09-13 21:08:54 +0600

    improve code

--- app/Http/Controllers/NewsAndArticleController.php
+++ app/Http/Controllers/NewsAndArticleController.php
@@ -15,23 +15,12 @@ use Illuminate\Support\Facades\Http;
 
 class NewsAndArticleController extends Controller
 {
-    protected $newsAndArticleObj;
-
-    public function __construct()
+    public function ifNullAssign($field)
     {
-        //NewsAndArticleInterface $newsAndArticleDi
-        //$this->newsAndArticleObj = $newsAndArticleDi;
-    }
-
-    function removeSpecialChar($str) {
-
-        // Using str_replace() function
-        // to replace the word
-        $res = str_replace( array( '\'', '"',
-            ',' , ';', '<', '>' ), ' ', $str);
-
-        // Returning the result
-        return $res;
+        if ($field == 'null') {
+            $field = null;
+        }
+        return $field;
     }
     /**
      * Display a listing of the resource.
@@ -43,16 +32,23 @@ class NewsAndArticleController extends Controller
 
         $query = NewsAndArticle::query();
 
-        if ($request['title'] != "null") {
+        $request['title'] = $this->ifNullAssign($request['title']);
+        $request['from'] = $this->ifNullAssign($request['from']);
+        $request['to'] = $this->ifNullAssign($request['to']);
+
+        if ($request['title']) {
             $query = $query->where('title', 'LIKE', "%$request->title%");
         }
 
-        if ($request->from != "null") {
+        if ($request->from) {
 
-            if ($request->to != "null") {
+            if ($request->to) {
                 $query = $query->whereBetween('publish_date',  [$request->from, $request->to]);
             }
         }
+
+        //return $query->latest()->get();
+        //dd($query->latest()->get());
 
         if ($personalizeProfile && $personalizeProfile->status == 'on') {
             if ($request->filterType == 'source') {
@@ -72,51 +68,7 @@ class NewsAndArticleController extends Controller
      */
     public function create()
     {
-        $endpoint = "https://newsapi.org/v2/everything?q=Apple&from=2023-09-11&sortBy=popularity&apiKey=572294b3723746ebbbe9ba2ce111f8bb";
-        $response = Http::get($endpoint);
-        $json =  json_decode($response, true);
-
-
-        $sourceArr = NewsSource::pluck('source_name')->toArray();
-        $authorArr = NewsAuthor::pluck('author_name')->toArray();
-
-        foreach ($json['articles'] as $item) {
-            $newsAndArticle = new NewsAndArticle();
-            $newsAndArticle->img                    = $item['urlToImage'];
-            $newsAndArticle->title                  = $item['title'];
-            $newsAndArticle->short_description      = $item['description'];
-            $newsAndArticle->description            = $item['content'];
-            $newsAndArticle->category               = 'not found';
-            $newsAndArticle->author                 = $this->removeSpecialChar($item['author']);
-            $newsAndArticle->source                 = $this->removeSpecialChar($item['source']['name']);
-            $newsAndArticle->publish_date           = $item['publishedAt'];
-
-            $sourceArr[] = $this->removeSpecialChar($item['source']['name']);
-            $authorArr[] = $this->removeSpecialChar($item['author']);
-
-            //echo $item['title'] . '<br>';
-            $newsAndArticle->save();
-        }
-
-        $sourceArr = array_unique($sourceArr);
-
-        foreach ($sourceArr as $source) {
-            $newsSource = new NewsSource();
-            $newsSource->source_name = $source;
-            $newsSource->save();
-        }
-
-        $authorArr = array_unique($authorArr);
-
-        //return ($authorArr);
-
-        foreach ($authorArr as $author) {
-            $newsAuthor = new NewsAuthor();
-            $newsAuthor->author_name = $author;
-            $newsAuthor->save();
-        }
-
-        return [$sourceArr, $authorArr];
+        //..
     }
 
     public function scrappingAndSave()
