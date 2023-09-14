f217d65054b03c4dfbee11873137cf0f7be000c2
tanvirs2 <tanvir603@gmail.com>
2023-09-14 18:43:28 +0600

    install command

---
+++ app/Console/Commands/FetchNewsAndArticle.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\DI_Processor\NewsAndArticleProcessor;
+use App\NewsPortals\Portal_NewsApiOrg;
+use App\NewsPortals\Portal_NewYorkTimes;
+use App\NewsPortals\Portal_TheGuardian;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Artisan;
+
+class FetchNewsAndArticle extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:fetch-news-and-article';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    public static function newsAndArticleSaveToDB()
+    {
+        (new NewsAndArticleProcessor(new Portal_NewsApiOrg))->saveToDB();
+        (new NewsAndArticleProcessor(new Portal_NewYorkTimes))->saveToDB();
+        (new NewsAndArticleProcessor(new Portal_TheGuardian))->saveToDB();
+    }
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //Artisan::call('migrate:fresh');
+        //Artisan::call('db:seed');
+        $this->newsAndArticleSaveToDB();
+    }
+}
